f581dcba39c31b0c1f3e680ca7063481c3dd9e7d
psi.carlosgil@gmail.com <carlosgdaries@gmail.com>
2026-03-30 18:01:36 +0200

    test constructor de cuenta

---
+++ tests/model/test_bank_account.py
@@ -0,0 +1,14 @@
+import pytest
+from model.bank_account import BankAccount
+
+@pytest.fixture
+def bank_account():
+    return BankAccount(1, 1, 123, 1000)
+
+
+def test_constructor(bank_account):
+    assert isinstance(bank_account, BankAccount)
+    assert bank_account.account_id == 1
+    assert bank_account.client_id == 1
+    assert bank_account.account_number == 123
+    assert bank_account.balance == 1000
