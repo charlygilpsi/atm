a1f191eaf3af853fa70ffabf47fc50474f883f44
psi.carlosgil@gmail.com <carlosgdaries@gmail.com>
2026-03-30 18:03:46 +0200

    test check balance

--- tests/model/test_bank_account.py
+++ tests/model/test_bank_account.py
@@ -12,3 +12,7 @@ def test_constructor(bank_account):
     assert bank_account.client_id == 1
     assert bank_account.account_number == 123
     assert bank_account.balance == 1000
+    
+
+def test_check_balance(bank_account):
+    assert bank_account.check_balance() == 1000
