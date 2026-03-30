9ac865425f28e28698e6b0fe0449d8e392ef0a63
psi.carlosgil@gmail.com <carlosgdaries@gmail.com>
2026-03-30 18:51:29 +0200

    test constructor

--- tests/model/test_transaction.py
+++ tests/model/test_transaction.py
@@ -6,9 +6,20 @@ from model.transaction import Transaction
 @pytest.fixture
 def transaction():
     atm = ATM(1000)
-    bank_account = BankAccount(1, 1, 123, 1000)
+    bank_account = BankAccount(1, 2, 123, 900)
     
     return Transaction(atm, bank_account)
+
+
+def test_constructor(transaction):
+    assert isinstance(transaction, Transaction)
+    assert isinstance(transaction.atm, ATM)
+    assert isinstance(transaction.bank_account, BankAccount)
+    assert transaction.atm.stock == 1000
+    assert transaction.bank_account.account_id == 1
+    assert transaction.bank_account.account_number == 123
+    assert transaction.bank_account.client_id == 2
+    assert transaction.bank_account.balance == 900
 
 
 def test_withdraw_cash_from_atm(transaction):
