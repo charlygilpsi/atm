4d23349730327416294e0bd2b65e07f3c871e543
psi.carlosgil@gmail.com <carlosgdaries@gmail.com>
2026-03-31 16:08:04 +0200

    cambiados tests para pasar amount como parámetro a transaction

--- tests/model/test_transaction.py
+++ tests/model/test_transaction.py
@@ -8,7 +8,17 @@ def transaction():
     atm = ATM(1000)
     bank_account = BankAccount(1, 2, 123, 900)
     
-    return Transaction(atm, bank_account)
+    return Transaction(atm, bank_account, 400)
+
+
+@pytest.fixture
+def atm():
+    return ATM(1000)
+
+
+@pytest.fixture
+def bank_account():
+    return BankAccount(1, 2, 123, 900)
 
 
 def test_constructor(transaction):
@@ -16,6 +26,7 @@ def test_constructor(transaction):
     assert isinstance(transaction.atm, ATM)
     assert isinstance(transaction.bank_account, BankAccount)
     assert transaction.atm.stock == 1000
+    assert transaction.amount == 400
     assert transaction.bank_account.account_id == 1
     assert transaction.bank_account.account_number == 123
     assert transaction.bank_account.client_id == 2
@@ -23,22 +34,19 @@ def test_constructor(transaction):
 
 
 def test_withdraw_cash_from_atm(transaction):
-    transaction.withdraw_cash_from_atm(100)
-    assert transaction.atm.stock == 900
-    
-    transaction.withdraw_cash_from_atm(300)
+    transaction.withdraw_cash_from_atm()
     assert transaction.atm.stock == 600
+    
+    transaction.withdraw_cash_from_atm()
+    assert transaction.atm.stock == 200
     
     
 def test_debit_from_account(transaction):
-    transaction.debit_from_account(100)
-    assert transaction.bank_account.balance == 800
+    transaction.debit_from_account()
+    assert transaction.bank_account.balance == 500
     
-    transaction.debit_from_account(600)
-    assert transaction.bank_account.balance == 200
-    
-    transaction.debit_from_account(200)
-    assert transaction.bank_account.balance == 0
+    transaction.debit_from_account()
+    assert transaction.bank_account.balance == 100
     
 
 @pytest.mark.parametrize("amount, expected_result", [
@@ -47,8 +55,8 @@ def test_debit_from_account(transaction):
     (1100, False),
     (5000, False)
 ])
-def test_validate_withdraw_cash_from_atm(transaction, amount, expected_result):
-    assert transaction.validate_withdraw_cash_from_atm(amount) == expected_result
+def test_validate_withdraw_cash_from_atm(atm, bank_account, amount, expected_result):
+    assert Transaction(atm, bank_account, amount).validate_withdraw_cash_from_atm() == expected_result
     
 
 @pytest.mark.parametrize("amount, expected_result", [
@@ -57,8 +65,8 @@ def test_validate_withdraw_cash_from_atm(transaction, amount, expected_result):
     (950, False),
     (1100, False)
 ])
-def test_validate_debit_from_account(transaction, amount, expected_result):
-    assert transaction.validate_debit_from_account(amount) == expected_result
+def test_validate_debit_from_account(atm, bank_account, amount, expected_result):
+    assert Transaction(atm, bank_account, amount).validate_debit_from_account() == expected_result
     
 
 @pytest.mark.parametrize("amount, expected_result", [
@@ -67,5 +75,5 @@ def test_validate_debit_from_account(transaction, amount, expected_result):
     (5, False),
     (10.5, False)
 ])
-def test_validate_amount_is_multiple_of_ten(transaction, amount, expected_result):
-    assert transaction.validate_amount_is_multiple_of_ten(amount) == expected_result
+def test_validate_amount_is_multiple_of_ten(atm, bank_account, amount, expected_result):
+    assert Transaction(atm, bank_account, amount).validate_amount_is_multiple_of_ten() == expected_result
